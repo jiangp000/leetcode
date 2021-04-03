5fee37f5216aee10fc4bf5e55ace726a4953526e
jiangp000 <1727237202@qq.com>
2021-04-03 21:47:44 +0800

    Q

---
+++ stack/src/MinStack155/MinStack155.java
@@ -0,0 +1,44 @@
+package MinStack155;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.Stack;
+
+public class MinStack155 {
+}
+class MinStack {
+    private Stack<Integer> stack;
+    private int small;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        small = Integer.MIN_VALUE;
+    }
+
+    public void push(int val) {
+        stack
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
